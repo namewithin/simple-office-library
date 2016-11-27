05859ed845999e143f8ce747af16a1d693582758
Leonid Korniienko <leo@pdffiller.com>
2016-11-27 12:51:37 +0200

    add JWT

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          */
 
         Laravel\Socialite\SocialiteServiceProvider::class,
+        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -177,6 +178,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
 
     ],
 
@@ -227,6 +229,8 @@ return [
         'View'         => Illuminate\Support\Facades\View::class,
 //
         'Socialite'    => Laravel\Socialite\Facades\Socialite::class,
+        'JWTAuth'      => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory'   => Tymon\JWTAuth\Facades\JWTFactory::class
     ],
 
 ];
